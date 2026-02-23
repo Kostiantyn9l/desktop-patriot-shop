33219664c397edb057b10f976e3c94fb08ebd4ca
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-02-23 21:54:39 +0200

    refactor: add dotenv import and express.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
+import "dotenv/config";
 import express, {type Express, type Request, type Response} from "express"
 
-const app: Express = express();
+const app:Express = express();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Working")
-})
+app.use(express.json());
+
+app.get("/", (req:Request, res:Response) => {
+    res.send("Working");
+});
 
 app.listen(PORT, () => {
-    console.log("App working")
-})
+    console.log(`Server started on port: ${PORT}`);
+});
