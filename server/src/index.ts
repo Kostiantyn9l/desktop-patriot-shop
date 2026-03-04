8ca610b7a8fddc4ce794fbfdb633029494b9e346
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-04 23:56:00 +0200

    refactor: wrap server start in async function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,26 @@
 import "dotenv/config";
-import express, {type Express, type Request, type Response} from "express"
+import express, {type Express, type Request, type Response} from "express";
+import cors from "cors";
+import { prisma } from "./lib/prisma.js";
 
 const app:Express = express();
-
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req:Request, res:Response) => {
-    res.send("Working");
-});
+const start = async () => {
+  try{
+   await prisma.$connect();
+   console.log("DB connected");
 
-app.listen(PORT, () => {
+   app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
-});
+   });
+  }catch(e){
+    console.log("Server failed to start", e);
+    process.exit(1);
+  }
+};
+
+start();
