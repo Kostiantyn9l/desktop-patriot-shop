035b5f4c959a914b25575ace467b606a3357f495
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-06 23:46:50 +0200

    feat: implement create and getAll brand controller methods

--- server/src/controllers/brandController.ts
+++ server/src/controllers/brandController.ts
@@ -1,12 +1,18 @@
 import { type Request, type Response } from "express";
+import prisma from "../lib/prisma.js";
 
 class brandController {
     async create(req: Request, res: Response) {
-
+        const { name } = req.body;
+        const brand = await prisma.brand.create({
+            data: { name }
+        });
+        return res.json(brand);
     }
 
     async getAll(req: Request, res: Response) {
-        
+        const brands = await prisma.brand.findMany();
+        return res.json(brands);
     }
 }
 
