7b48be13fce1ca0c43eca8dbac91371a3bec3078
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-06 16:59:44 +0200

    feat: add brand router and controller

---
+++ server/src/controllers/brandController.ts
@@ -0,0 +1,13 @@
+import { type Request, type Response } from "express";
+
+class brandController {
+    async create(req: Request, res: Response) {
+
+    }
+
+    async getAll(req: Request, res: Response) {
+        
+    }
+}
+
+export default new brandController;
