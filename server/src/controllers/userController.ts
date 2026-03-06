2097f06ec023ae33123ba0daf7217c390ec45f5d
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-06 16:59:25 +0200

    feat: add user router and controller

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,17 @@
+import { type Request, type Response } from "express";
+
+class userController {
+    async registration(req: Request, res: Response) {
+
+    }
+
+    async login(req: Request, res: Response) {
+        
+    }
+
+    async auth(req: Request, res: Response) {
+        
+    }
+}
+
+export default new userController;
