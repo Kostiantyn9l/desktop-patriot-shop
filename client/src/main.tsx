8c5a5243fcc58668576163d66a7cf322bec8564d
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-19 23:32:42 +0200

    feat: add ContextValue interface and improve Context type safety

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import { createContext, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import UserStore from './store/UserStore.ts';
+import WeaponStore from './store/WeaponStore.tsx';
+
+interface ContextValue {
+  user: UserStore;
+  weapon: WeaponStore;
+}
+
+export const Context = createContext<ContextValue | null>(null);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Context.Provider value={{
+    user: new UserStore(),
+    weapon: new WeaponStore()
+  }}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Context.Provider>
 )
