d52ff1bae080710727bd95e78533e9d8969dc06a
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-19 23:32:11 +0200

    feat: add exported type definitions (Type, Brand, Weapon, User)

---
+++ client/src/types/types.ts
@@ -0,0 +1,23 @@
+export interface Type {
+    id: number;
+    name: string;
+}
+
+export interface Brand {
+    id: number;
+    name: string;
+}
+
+export interface Weapon {
+    id: number;
+    name: string;
+    price: number;
+    rating: number;
+    img: string;
+}
+
+export interface User {
+    id?: number;
+    email?: string;
+    role?: string;
+}
