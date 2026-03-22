0a2863e23493907ba74513eb4ed89584dc8ae158
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-22 21:47:07 +0200

    feat: add Footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react";
+
+const Header: FC = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 Patriot. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Header;
