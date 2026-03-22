4e76f7c64e94ed996fa8d5babfae9455f8e0b5e0
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-22 21:46:54 +0200

    feat: add Layout component and make AppRouter observable

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -2,8 +2,10 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { authRoutes, errorRoutes, publicRoutes } from "../routes";
 import { useContext, type FC } from "react";
 import { Context } from "../main";
+import Layout from "./Layout";
+import { observer } from "mobx-react-lite";
 
-const AppRouter: FC = () => {
+const AppRouter: FC = observer(() => {
     const context = useContext(Context);
     if(!context) {
         throw new Error("Context not provided");
@@ -20,13 +22,15 @@ const AppRouter: FC = () => {
 
     return (
         <Routes>
-            {routes.map(({path, Component}) => (
-                <Route key={path} path={path} element={<Component />} />
-            ))}
+            <Route element={<Layout />}>
+                {routes.map(({path, Component}) => (
+                    <Route key={path} path={path} element={<Component />} />
+                ))}
+            </Route>
 
             <Route path="*" element={<Navigate to={'/404'} />}/>
         </Routes>
     );
-};
+});
 
 export default AppRouter;
