d48e9843d0722faa3ac8eb79802854f93da4c00e
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-24 23:05:49 +0200

    feat(admin): integrate Admin Modals into Admin page

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+import CreateBrand from "../components/modals/adminModals/CreateBrandModal";
+import CreateCategory from "../components/modals/adminModals/CreateCategoryModal";
+import CreateWeapon from "../components/modals/adminModals/CreateWeaponModal";
+
 const Admin = () => {
+    const [categoryShow, setCategoryShow] = useState(false);
+    const [brandShow, setBrandShow] = useState(false);
+    const [weaponShow, setWeaponShow] = useState(false);
+
     return (
-        <div>
-            ADMIN
-        </div>
+        <section>
+            <h1>Адміністрування</h1>
+            <div>
+                <button
+                    onClick={() => setCategoryShow(true)}
+                >
+                    Додати Категорію
+                </button>
+                <button 
+                    onClick={() => setBrandShow(true)}
+                >
+                    Додати бренд
+                </button>
+                <button 
+                    onClick={() => setWeaponShow(true)}
+                >
+                    Додати Озброєння
+                </button>
+            </div>
+
+            <CreateCategory show={categoryShow} onClose={() => setCategoryShow(false)}/>
+            <CreateBrand show={brandShow} onClose={() => setBrandShow(false)}/>
+            <CreateWeapon show={weaponShow} onClose={() => setWeaponShow(false)}/>
+        </section>
     )
 }
 
