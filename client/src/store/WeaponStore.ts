9a22afb3861a2659c60af4b4a2c7836d573ef8c6
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-24 22:41:42 +0200

    feat: add Selected Type and Selected Brand

--- client/src/store/WeaponStore.ts
+++ client/src/store/WeaponStore.ts
@@ -3,17 +3,23 @@ import type { Brand, Type, Weapon } from "../types/types";
 
 export default class WeaponStore {
     private _types: Type[] = [
-        {id: 1, name: 'Lox'},
-        {id: 2, name: 'Popa'}
+        {id: 1, name: 'Зброя та патрони'},
+        {id: 2, name: 'Оптика та кріплення'},
+        {id: 3, name: 'Ножі та інструменти'},
+        {id: 4, name: 'Догляд та зберігання'},
+        {id: 5, name: 'Екіпірування'},
+        {id: 6, name: 'Самозахист'}
     ];
     private _brands: Brand[] = [
-        {id: 1, name: 'ishowspeed'},
-        {id: 2, name: 'ishowboobie'}
+        {id: 1, name: 'A&F'},
+        {id: 2, name: 'Air Pistol'}
     ];
     private _weapons: Weapon[] = [
         {id: 1, name: 'tratata', price: 25000, rating: 5, img: 'https'},
         {id: 2, name: 'boom', price: 25000, rating: 5, img: 'https'}
     ];
+    private _selectedType: Type = {};
+    private _selectedBrand: Brand = {};
 
     constructor() {
         makeAutoObservable(this);
@@ -22,9 +28,18 @@ export default class WeaponStore {
     setTypes(types: Type[]): void{
         this._types = types
     }
+
     setBrands(brands: Brand[]): void {
         this._brands = brands
     }
+
+    setSelectedType(type: Type): void {
+        this._selectedType = type
+    }
+    setSelectedBrand(brand: Brand): void {
+        this._selectedBrand = brand
+    }
+
     setWeapons(weapons: Weapon[]): void {
         this._weapons = weapons
     }
@@ -32,10 +47,20 @@ export default class WeaponStore {
     get types(): Type[] {
         return this._types;
     }
+
     get brands(): Brand[] {
         return this._brands;
     }
+
     get weapons(): Weapon[] {
         return this._weapons;
+    }
+
+    get type(): Type {
+        return this._selectedType;
+    }
+
+    get brand(): Brand {
+        return this._selectedBrand;
     }
 }
