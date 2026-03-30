95c204805f471d77ab5a69ec0c4e7e67f26acf9d
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-31 00:15:10 +0300

    feat(entities): remove token in clearAuth

--- client/src/entities/user/model/UserStore.ts
+++ client/src/entities/user/model/UserStore.ts
@@ -48,6 +48,7 @@ export default class UserStore {
         this._user = null;
         localStorage.removeItem("isAuth");
         localStorage.removeItem("user");
+        localStorage.removeItem("token");
     }
 
     get isAuth(): boolean {
