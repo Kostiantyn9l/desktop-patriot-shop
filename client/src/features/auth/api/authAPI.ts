88b873386ae0d462408953986407606dae81e094
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-31 00:13:28 +0300

    feat(features): implement JWT storage, decoding and auth check

--- client/src/features/auth/api/authAPI.ts
+++ client/src/features/auth/api/authAPI.ts
@@ -1,16 +1,34 @@
 import { $host, $authHost } from "../../../shared/api";
+import { jwtDecode } from "jwt-decode";
+
+interface JwtPayload {
+    id: number,
+    role: string
+}
 
 export const registration = async (name: string, email: string, password: string) => {
     const { data } = await $host.post('api/user/registration', { name, email, password });
-    return data;
+    localStorage.setItem('token', data.token);
+    return { 
+        ...data, 
+        decodedToken: jwtDecode<JwtPayload>(data.token)
+    };
 }
 
 export const login = async (email: string, password: string) => {
     const { data } = await $host.post('api/user/login', { email, password });
-    return data;
+    localStorage.setItem('token', data.token);
+    return { 
+        ...data, 
+        decodedToken: jwtDecode<JwtPayload>(data.token)
+    };
 }
 
 export const checkAuth = async () => {
     const { data } = await $authHost.get('api/user/auth');
-    return data;
+    localStorage.setItem('token', data.token);
+    return { 
+        ...data, 
+        decodedToken: jwtDecode<JwtPayload>(data.token)
+    };
 }
