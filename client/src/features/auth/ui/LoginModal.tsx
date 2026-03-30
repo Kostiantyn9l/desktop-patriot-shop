fb3f7c984b4a62128297079d2f8bcf87eed42b00
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-31 00:04:50 +0300

    feat(features): handle user login and register in LoginModal, RegisterModal

--- client/src/features/auth/ui/LoginModal.tsx
+++ client/src/features/auth/ui/LoginModal.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import { useStore } from "../../../shared/hooks/useStore";
+import { login } from "../api/authAPI";
+import { observer } from "mobx-react-lite";
+import axios from "axios";
 
 interface LoginModalProps {
     onSwitchToRegister: () => void;
     onClose: () => void;
 }
 
-const LoginModal: React.FC<LoginModalProps> = ({ onSwitchToRegister, onClose }) => {
+const LoginModal: React.FC<LoginModalProps> = observer(({ onSwitchToRegister, onClose }) => {
     const { user } = useStore();
 
-    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        user.setIsAuth(true);
-        onClose();
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+        try { 
+            e.preventDefault();
+
+            const data = await login(email, password);
+            console.log(data);
+
+            user.setUser(data.user)
+            user.setIsAuth(true);
+            onClose();
+        } catch (err) { 
+            if (axios.isAxiosError(err)) {
+                alert(err.response?.data.message || "Помилка");
+            }
+        }
     };
 
     return (
@@ -20,11 +38,13 @@ const LoginModal: React.FC<LoginModalProps> = ({ onSwitchToRegister, onClose }) 
 
             <form className="auth-form" onSubmit={handleSubmit}>
                 <label>
-                    <span>Ім'я користувача</span>
+                    <span>Email</span>
                     <input
-                        name="username"
-                        type="text"
-                        placeholder="Введіть ім'я"
+                        name="email"
+                        type="email"
+                        placeholder="Введіть email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </label>
 
@@ -34,15 +54,17 @@ const LoginModal: React.FC<LoginModalProps> = ({ onSwitchToRegister, onClose }) 
                         name="password"
                         type="password"
                         placeholder="Введіть пароль"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </label>
 
-                <button type="submit" >Увійти</button>
+                <button type="submit">Увійти</button>
             </form>
 
             <p>Немає аккаунта? <button onClick={onSwitchToRegister}>Зареєструватися</button></p>
         </div>
     );
-};
+});
 
 export default LoginModal;
--- client/src/features/auth/ui/RegisterModal.tsx
+++ client/src/features/auth/ui/RegisterModal.tsx
@@ -1,28 +1,38 @@
 import { useState } from "react";
 import { registration } from "../api/authAPI";
 import { useStore } from "../../../shared/hooks/useStore";
+import { observer } from "mobx-react-lite";
+import axios from "axios";
 
 interface RegisterModalProps {
     onSwitchToLogin: () => void;
     onClose: () => void;
 }
 
-const RegisterModal: React.FC<RegisterModalProps> = ({ onSwitchToLogin, onClose }) => {
+const RegisterModal: React.FC<RegisterModalProps> = observer(({ onSwitchToLogin, onClose }) => {
     const { user } = useStore();
 
     const [name, setName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     
-    const signIn = async () => {
-        const response = await registration(name, email, password);
-        console.log(response);
-    }
+    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+        try {
+            e.preventDefault();
 
-    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        user.setIsAuth(true);
-        onClose();
+            let data = null;
+            data = await registration(name, email, password);
+
+            user.setUser(data.user)
+            user.setIsAuth(true);
+            console.log(data);
+
+            onClose();   
+        } catch (err) { 
+            if (axios.isAxiosError(err)) {
+                alert(err.response?.data.message || "Помилка");
+            }
+        }
     };
 
     return (
@@ -63,12 +73,12 @@ const RegisterModal: React.FC<RegisterModalProps> = ({ onSwitchToLogin, onClose 
                     />
                 </label>
 
-                <button type="submit" onClick={signIn}>Зареєструватися</button>
+                <button type="submit">Зареєструватися</button>
             </form>
 
             <p>Вже є аккаунт? <button onClick={onSwitchToLogin}>Увійти</button></p>
         </div>
     );
-};
+});
 
 export default RegisterModal;
