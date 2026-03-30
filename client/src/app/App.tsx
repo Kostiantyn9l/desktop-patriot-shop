12c17e6c84fe837295e799406f5e5b9c1b4c1c8e
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-03-31 00:29:17 +0300

    feat(app): restore user session after page refresh

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,26 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRouter from "./AppRouter"
+import { useEffect, useState } from "react";
+import { checkAuth } from "../features/auth/api/authAPI";
+import { useStore } from "../shared/hooks/useStore";
 
 function App() {
+  const { user } = useStore();
+
+  const [loading, setLoading] = useState<boolean>(true);
+  
+  useEffect(() => {
+    checkAuth().then( data => {
+        user.setUser(data.user);
+        user.setIsAuth(true);
+      })
+      .finally(() => setLoading(false))
+  }, []);
+
+  if (loading) {
+    return null;
+  };
+
   return (
     <BrowserRouter>
       <AppRouter />
