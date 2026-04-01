19ba28716bc41adb1fc6bef208b580b1d83aadcf
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-01 22:04:45 +0300

    feat(pages): implement get API logic to Shop and WeaponPage

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,8 +1,20 @@
 import BrandSidebar from "../widgets/BrandSidebar/BrandSidebar";
 import CategorySidebar from "../widgets/CategorySidebar/CategorySidebar";
 import WeaponList from "../entities/weapon/ui/WeaponList";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../shared/hooks/useStore";
+import { useEffect } from "react";
+import { fetchBrands, fetchTypes, fetchWeapons } from "../features/weapon/ai/weaponApi";
 
-const Shop = () => {
+const Shop = observer(() => {
+    const { weapon } = useStore();
+
+    useEffect(() => { 
+        fetchTypes().then(data => weapon.setTypes(data))
+        fetchBrands().then(data => weapon.setBrands(data))
+        fetchWeapons().then(data => weapon.setWeapons(data.row))
+    }, [weapon]);
+
     return (
         <div>
             <CategorySidebar />
@@ -10,6 +22,6 @@ const Shop = () => {
             <WeaponList />
         </div>
     )
-}
+});
 
 export default Shop;
--- client/src/pages/WeaponPage.tsx
+++ client/src/pages/WeaponPage.tsx
@@ -1,26 +1,27 @@
 import type { Weapon } from "../shared/types/types";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-interface Description {
-    id: number;
-    title: string;
-    description: string;
-}
+import { fetchOneWeapon } from "../features/weapon/ai/weaponApi";
+import { Spinner } from "../shared/ui/Spinner/Spinner";
 
 const WeaponPage = () => {
+    const [weapon, setWeapon] = useState<Weapon | null>(null);
     const { id } = useParams<{ id: string }>();
-    const weapon: Weapon = {id: parseInt(id || '1'), name: "AK-47", price: 1000, rating: 5, img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/AK-47_type_II_Part_DM-ST-89-01131.jpg/2560px-AK-47_type_II_Part_DM-ST-89-01131.jpg"};
-    const description: Description[] = [
-        {id: 1, title: "Магазин", description: "30 патронів"},
-        {id: 2, title: "Калібр", description: "7.62 мм"},
-        {id: 3, title: "Довжина", description: "870 мм"},
-        {id: 4, title: "Вага", description: "3.47 кг"}
-    ]
+
+    useEffect(() => {
+        if (!id) return;
+        const weaponId = Number(id);
+        fetchOneWeapon(weaponId).then(data =>  setWeapon(data))
+    }, [id])
+
+    if (!weapon) {
+        return <Spinner />
+    };
 
     return (
         <section>
             <div>
-                <img src={weapon.img} alt={weapon.name} />
+                <img src={import.meta.env.VITE_REACT_APP_API_URL + weapon.img} alt={weapon.name} />
                 <div>
                     <h2>{weapon.name}</h2>
                 </div>
@@ -34,7 +35,7 @@ const WeaponPage = () => {
 
             <div>
                 <h1>Характеристики</h1>
-                {description.map((info, index: number) =>
+                {weapon.info.map((info, index: number) =>
                     <ul key={info.id} style={index % 2 === 0 ? { backgroundColor: "lightgray" } : { backgroundColor: "darkgray" }}>
                         <li>{info.title}: {info.description}</li>
                     </ul>
