c72fb598a4ede32705173298f4a274af8347ed87
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-02 22:07:35 +0300

    feat(features): add params to fetchWeapons

--- client/src/features/weapon/api/weaponApi.ts
+++ client/src/features/weapon/api/weaponApi.ts
@@ -1,4 +1,5 @@
 import { $host, $authHost } from "../../../shared/api";
+import { type FetchWeaponsParams } from "../../../shared/types/types";
 
 export const createType = async (type: string) => {
     const { data } = await $authHost.post('api/type/', { name: type });
@@ -25,8 +26,18 @@ export const createWeapon = async (weaponData: FormData) => {
     return data;
 }
 
-export const fetchWeapons = async () => {
-    const { data } = await $host.get('api/weapon/');
+export const fetchWeapons = async (
+    typeId?: number, 
+    brandId?: number, 
+    page: number = 1, 
+    limit: number = 3, 
+) => {
+    const params: FetchWeaponsParams = { page, limit }
+
+    if (typeId !== undefined) params.typeId = typeId;
+    if (brandId !== undefined) params.brandId = brandId;
+
+    const { data } = await $host.get('api/weapon/', { params });
     return data;
 }
  
