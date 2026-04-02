3f6c7b53c020266e71cd1bc99212e63c82c94a75
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-02 22:12:39 +0300

    feat(pages): add useEffect for pagination and implement Pagination

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -4,7 +4,8 @@ import WeaponList from "../entities/weapon/ui/WeaponList";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../shared/hooks/useStore";
 import { useEffect } from "react";
-import { fetchBrands, fetchTypes, fetchWeapons } from "../features/weapon/ai/weaponApi";
+import { fetchBrands, fetchTypes, fetchWeapons } from "../features/weapon/api/weaponApi";
+import Pagination from "../shared/ui/pagination/Pagination";
 
 const Shop = observer(() => {
     const { weapon } = useStore();
@@ -12,14 +13,39 @@ const Shop = observer(() => {
     useEffect(() => { 
         fetchTypes().then(data => weapon.setTypes(data))
         fetchBrands().then(data => weapon.setBrands(data))
-        fetchWeapons().then(data => weapon.setWeapons(data.row))
-    }, [weapon]);
+        fetchWeapons().then(data => {
+            weapon.setWeapons(data.row)
+            weapon.setTotalCount(data.count)
+        })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => { 
+        fetchWeapons(weapon.type?.id, weapon.brand?.id, weapon.page, weapon.limit)
+        .then(data => {
+            weapon.setWeapons(data.row)
+            weapon.setTotalCount(data.count)
+        })
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [weapon.page, weapon.type?.id, weapon.brand?.id])
 
     return (
         <div>
-            <CategorySidebar />
-            <BrandSidebar />
-            <WeaponList />
+            <div>
+                <CategorySidebar />
+                <BrandSidebar />
+            </div>
+            <div>
+                <WeaponList />
+                <Pagination 
+                    totalCount={weapon.totalCount}
+                    limit={weapon.limit}
+                    page={weapon.page}
+                    onChange={(p) => weapon.setPage(p)}
+                />
+            </div>
         </div>
     )
 });
