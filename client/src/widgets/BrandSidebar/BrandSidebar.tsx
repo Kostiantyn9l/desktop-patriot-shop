2620f206faf5ea31fd7e7e8080b59cbcb55a44e7
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-04 07:56:28 +0300

    style(widgets): add CSS Module styles to sidebar

--- client/src/widgets/BrandSidebar/BrandSidebar.tsx
+++ client/src/widgets/BrandSidebar/BrandSidebar.tsx
@@ -1,24 +1,25 @@
 import { observer } from "mobx-react-lite";
 import { type FC } from "react";
 import { useStore } from "../../shared/hooks/useStore";
-import "./BrandSidebar.scss";
+import style from "./BrandSidebar.module.scss";
 
 const BrandSidebar: FC = observer(() => {
     const { weapon } = useStore();
 
     return (
-        <aside className="brand-sidebar">
-            <ul className="brand-sidebar__list">
-                { weapon.brands.map((brand) => {
+        <aside className={style.brandSidebar}>
+            <h3>Бренди</h3>
+            <ul className={style.brandSidebar__list}>
+                {weapon.brands.map((brand) => {
                     const isActive = brand.id === weapon.brand?.id;
 
                     return (
-                        <li key={brand.id} className="brand-sidebar__item">
+                        <li key={brand.id} className={style.brandSidebar__item}>
                             <button
-                                className={`brand-sidebar__button ${
-                                    isActive ? "brand-sidebar__button--active" : ""
-                                }`}
-                                onClick={() => weapon.setSelectedBrand(brand)} 
+                                className={`${style.brandSidebar__button} ${
+                                isActive ? style.brandSidebar__buttonActive : ""
+                            }`}
+                                onClick={() => weapon.setSelectedBrand(brand)}
                             >
                                 {brand.name}
                             </button>
--- client/src/widgets/CategorySidebar/CategorySidebar.tsx
+++ client/src/widgets/CategorySidebar/CategorySidebar.tsx
@@ -1,29 +1,30 @@
 import { observer } from "mobx-react-lite";
 import { type FC } from "react";
 import { useStore } from "../../shared/hooks/useStore";
-import "./CategorySidebar.scss";
+import style from "./CategorySidebar.module.scss";
 
 const CategorySidebar: FC = observer(() => {
     const { weapon } = useStore();
 
     return (
-        <aside className="category-sidebar">
-            <ul className="category-sidebar__list">
+        <aside className={style.categorySidebar}>
+            <h3>Категорії</h3>
+            <ul className={style.categorySidebar__list}>
                 {weapon.types.map((type) => {
                     const isActive = type.id === weapon.type?.id;
 
-                    return(
-                        <li key={type.id} className="category-sidebar__item">
+                    return (
+                        <li key={type.id} className={style.categorySidebar__item}>
                             <button
-                                className={`category-sidebar__button ${
-                                    isActive ? "category-sidebar__button--active" : ""
-                                }`}
+                                className={`${style.categorySidebar__button} ${
+                                isActive ? style.categorySidebar__buttonActive : ""
+                            }`}
                                 onClick={() => weapon.setSelectedType(type)}
                             >
                                 {type.name}
                             </button>
                         </li>
-                    )
+                    );
                 })}
             </ul>
         </aside>
