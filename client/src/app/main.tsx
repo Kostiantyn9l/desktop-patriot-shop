b49999af645b93f10c5c9a883d46bac79fa5620f
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-05 23:44:06 +0300

    feat(app, entities): add OrderStore and implement it in Context.Provider

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -5,11 +5,13 @@ import UserStore from '../entities/user/model/UserStore.ts';
 import WeaponStore from '../entities/weapon/model/WeaponStore.ts';
 import '../shared/styles/index.scss'
 import BasketStore from '../entities/basket/model/BasketStore.ts';
+import OrderStore from '../entities/order/model/OrderStore.ts';
 
 interface ContextValue {
   user: UserStore;
   weapon: WeaponStore;
   basket: BasketStore;
+  order: OrderStore;
 }
 
 export const Context = createContext<ContextValue | null>(null);
@@ -20,7 +22,8 @@ createRoot(document.getElementById('root')!).render(
   <Context.Provider value={{
     user: new UserStore(),
     weapon: new WeaponStore(),
-    basket: new BasketStore()
+    basket: new BasketStore(),
+    order: new OrderStore()
   }}>
     <StrictMode>
       <App />
---
+++ client/src/entities/order/model/OrderStore.ts
@@ -0,0 +1,62 @@
+import { makeAutoObservable, runInAction } from "mobx";
+import * as api from "../api/orderApi";
+import type { Order } from "../../../shared/types/types";
+
+export default class OrderStore {
+    private _orders: Order[] = [];
+    private _lastOrderCode: string | null = null;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    // Setters
+    setOrders(orders: Order[]) { this._orders = orders; }
+    setLastOrderCode(code: string | null) { this._lastOrderCode = code; }
+
+    clearLastOrderCode() {
+        this._lastOrderCode = null;
+    }
+
+    // Getters
+    get orders(): Order[] { return this._orders; }
+    get lastOrderCode(): string | null { return this._lastOrderCode; }
+
+    createOrder = async (): Promise<string | null> => {
+        try {
+            const data = await api.createOrder();
+
+            runInAction(() => {
+                this.setLastOrderCode(data.code);
+            });
+
+            await this.fetchOrders();
+
+            return data.code;
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    };
+
+    fetchOrders = async () => {
+        try {
+            const data = await api.getUserOrders();
+
+            runInAction(() => {
+                this.setOrders(data);
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    confirmOrder = async (orderId: number, code: string) => {
+        try {
+            await api.confirmOrder(orderId, code);
+            await this.fetchOrders();
+        } catch (e) {
+            console.error(e);
+        }
+    };
+}
