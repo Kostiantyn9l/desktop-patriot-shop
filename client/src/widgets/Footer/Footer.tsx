73123f6a179cc9f354f2407393a2a63b116f6aca
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-06 00:02:33 +0300

    style(widgets): add SCSS, CSS Module styles to Footer

--- client/src/widgets/Footer/Footer.tsx
+++ client/src/widgets/Footer/Footer.tsx
@@ -1,11 +1,43 @@
 import { type FC } from "react";
-import "./Footer.scss";
+import styles from "./Footer.module.scss";
+import { Link } from "react-router-dom";
+import { ORDERS_ROUTE, SHOP_ROUTE } from "../../shared/lib/consts";
 
 const Footer: FC = () => {
     return (
-        <footer>
-            <p>&copy; 2023 Patriot. All rights reserved.</p>
-        </footer>
+        <div className={styles.footerWrapper}>
+            <div className="container">
+                <footer className={styles.footer}>
+                    <div>
+                        <div>
+                            <h3>Patriot</h3>
+                            <p>We strive to be the best for our customers.</p>
+                        </div>
+
+                        <div>
+                            <h4>Навігація</h4>
+                            <ul>
+                                <Link to={SHOP_ROUTE}>Головна</Link>
+                                <Link to={ORDERS_ROUTE}>Замовлення</Link>
+                            </ul>
+                        </div>
+
+                        <div className={styles.footer__social}>
+                            <h4>Ми у соцмережах</h4>
+                            <ul>
+                                <li><a href="https://t.me/" target="_blank">Telegram</a></li>
+                                <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
+                                <li><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={styles.footer__bottom}>
+                        <p>&copy; 2023 Patriot. All rights reserved.</p>
+                    </div>
+                </footer>
+            </div>
+        </div>
     );
 };
 
