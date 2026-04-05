029d7224750d598c4dacd090172ec9ad8cbbb901
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-05 23:38:20 +0300

    feat(routes): add order router and orderRoute.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 
 import userRouter from "./userRouter.js";
-import weaponRouter from "./weaponRouter.js";
+import weaponRouter from "./weaponRouter.js"
 import typeRouter from "./typeRouter.js";
 import brandRouter from "./brandRouter.js";
 import basketRouter from "./basketRoutes.js";
+import orderRouter from "./orderRouter.js";
 
 const rootRouter: Router = Router();
 
@@ -13,5 +14,6 @@ rootRouter.use('/weapon', weaponRouter);
 rootRouter.use('/type', typeRouter);
 rootRouter.use('/brand', brandRouter);
 rootRouter.use('/basket', basketRouter);
+rootRouter.use('/order', orderRouter);
 
 export default rootRouter;
---
+++ server/src/routes/orderRouter.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import orderController from "../controllers/ordersController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+import checkRoleMiddleware from "../middleware/checkRoleMiddleware.js";
+
+const router = express.Router();
+
+router.post('/', authMiddleware, orderController.create);
+router.get('/', authMiddleware, orderController.getUserOrders);
+router.get('/all', checkRoleMiddleware("ADMIN"), orderController.getAll);
+router.post('/confirm', checkRoleMiddleware("ADMIN"), orderController.confirm);
+
+export default router;
