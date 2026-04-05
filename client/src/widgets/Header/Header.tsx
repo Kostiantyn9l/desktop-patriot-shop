0b4756bed8913d805aa3d5308aeadfa4fe16f68f
Kostiantyn9l <kostiantyn.lk@gmail.com>
2026-04-06 00:05:04 +0300

    feat(widgets): add Admin admin role check and Link to ORDER_ROUTE

--- client/src/widgets/Header/Header.tsx
+++ client/src/widgets/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { type FC, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
-import { ADMIN_ROUTE, SHOP_ROUTE } from "../../shared/lib/consts";
+import { ADMIN_ROUTE, ORDERS_ROUTE, SHOP_ROUTE } from "../../shared/lib/consts";
 import AuthModal from "../../features/auth/ui/AuthModal";
 import { useStore } from "../../shared/hooks/useStore";
 import styles from "./Header.module.scss"
@@ -30,18 +30,27 @@ const Header: FC = observer(() => {
 
                     {user.isAuth ? (
                         <nav className={styles.nav}>
-                            <Link to={ADMIN_ROUTE} className={styles.link}>
-                                Admin
+                            {user.user?.role === 'ADMIN' && (
+                                <>
+                                    <Link to={ADMIN_ROUTE} className={styles.link}>
+                                        Admin
+                                    </Link>
+                                </>
+                            )}
+
+                            <Link to={ORDERS_ROUTE} className={styles.link}>
+                                Замовлення
                             </Link>
 
                             <button
+                                className={styles.buttonPrimary}
                                 onClick={() => setIsBasketOpen(true)}
                             >
                                 Кошик
                             </button>
 
                             <button 
-                                className={styles.button}
+                                className={styles.buttonEscape}
                                 onClick={() => user.clearAuth()}
                             >
                                 Вийти
